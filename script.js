5cbacba239fc58a98854b00faf6212b967998126
Vedant <vedantnp200@gmail.com>
2023-07-06 21:58:30 +0530

    [Vedant]-validate name using regex in javascript

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }
+
+// Form validation using regex
+
+function validateForm(name) {
+
+    let nameRegex = /^[A-Z]{1}[a-z]{2,}$/;
+
+    if(!nameRegex.test(name)) {
+        alert("Invalid Name");
+        return false;
+    }
+
+    return true;
+
+}
+
+// Alert message
+
+document.getElementById('myForm').addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+
+    if (validateForm(name)) {
+        alert(`Form Submitted Successfully!\n\nName: ${name}`);
+    }
+
+});
