4a34726980b414fed60a98275e162f5d87fa27a9
Vedant <vedantnp200@gmail.com>
2023-07-06 22:09:01 +0530

    [Vedant]-Validate password using regex

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ function rangeSlide(value) {
 
 // Form validation using regex
 
-function validateForm(name) {
+function validateForm(name, pass) {
 
     let nameRegex = /^[A-Z]{1}[a-z]{2,}$/;
+    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-={}?|:;,.]).{8,32}$/;
 
-    if(!nameRegex.test(name)) {
+    if (!nameRegex.test(name)) {
         alert("Invalid Name");
+        return false;
+    }
+
+    if (!passwordRegex.test(pass)) {
+        alert("Invalid Password");
         return false;
     }
 
@@ -25,9 +31,10 @@ document.getElementById('myForm').addEventListener("submit", function(event){
     event.preventDefault();
 
     let name = document.getElementById('name').value;
+    let pass = document.getElementById('pass').value;
 
-    if (validateForm(name)) {
-        alert(`Form Submitted Successfully!\n\nName: ${name}`);
+    if (validateForm(name, pass)) {
+        alert(`Form Submitted Successfully!\n\nName: ${name}\n\nPassword: ${pass}`);
     }
 
 });
